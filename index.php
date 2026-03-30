6b212d22510acdd21f1f867ba8a8e99cc0dc5d62
LyckYz <ilya.kokin654@mail.ru>
2026-03-31 00:21:37 +0300

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-print("PHP is working!");
-$user = 'u82371';
-$pass = '5135832'; 
-$db = new PDO('mysql:host=localhost;dbname=u82371', $user, $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-?>
