99916e68bc5987a93a772022bff94462c4a46a55
LyckYz <ilya.kokin654@mail.ru>
2026-03-31 00:26:36 +0300

    Add files via upload

---
+++ l2/index.php
@@ -0,0 +1,9 @@
+<?php
+
+print("PHP is working!");
+$user = 'u82371';
+$pass = '5135832';
+$db = new PDO('mysql:host=localhost;dbname=u82371', $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
+
+?>
